43365337b8fb1c449ef8348a7acf17bd4a01653e
GOKULVCE <gokuljcse2021@gmail.com>
2025-09-10 12:20:21 +0530

    small change in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from"@clerk/express";
-import { inngest } from "./config/inngest.js";
+import { inngest,functions } from "./config/inngest.js";
 import { serve } from "inngest/express";
 
 
